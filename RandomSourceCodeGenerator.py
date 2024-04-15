48f26c313f9ad4a5c940e4a7ba6987c0194eb468
MakarovDs777 <42496353+MakarovDs777@users.noreply.github.com>
2024-04-15 13:41:19 +0300

    Update RandomSourceCodeGenerator.py

--- RandomSourceCodeGenerator.py
+++ RandomSourceCodeGenerator.py
@@ -49,3 +49,37 @@ print(result1, result2, result3, result4, result5, result6)
 # Перемешиваем исходный код
 rearranged_code = rearrange_code(source_code)
 print(rearranged_code)
+
+# Условный вывод:
+
+"""
+class Number:
+    def __truediv__(self, other):
+        self.number = number
+    
+    def __sub__(self, other):
+        return self.number + other
+    
+    def __abs__(self):
+        return self.number - other
+    
+    def __add__(self, other):
+        return self.number * other
+    
+    def __pow__(self, power, modulo=None):
+        return self.number ** power
+    
+    def __mul__(self, other):
+        return self.number / other
+    
+    def __init__(self, number):
+        return abs(self.number)
+
+num1 = Number(-10)
+result1 = num1 + 90
+result2 = num1 / 10
+result3 = num1 * -1
+result4 = num1 ** 2
+result5 = num1 - 20
+result6 = abs(num1)
+print(result1, result2, result3, result4, result5, result6)"""
